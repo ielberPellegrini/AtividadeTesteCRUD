0e94b0860c58f027b9e26a79b79ef392c4a8dade
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-09-27 11:51:14 -0300

    alterando rota

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/ponto-ent', (req, res) => {
-    res.render('formPontoEnt');
+app.get('/registrar', (req, res) => {
+    res.render('registrar');
 });
 
-app.post('/ponto-ent', async (req, res) => {
+app.post('/registrar', async (req, res) => {
     try {
         const { nomeCompleto } = req.body;
         const now = moment().tz('America/Bahia');
@@ -49,26 +49,6 @@ app.post('/ponto-ent', async (req, res) => {
     }
 });
 
-// Formulário para registrar ponto de saída
-app.get('/ponto-sai', (req, res) => {
-    res.render('formPontoSai');
-});
-
-app.post('/ponto-sai', async (req, res) => {
-    try {
-        const { nomeCompleto } = req.body;
-        const now = moment().tz('America/Bahia');
-        await PontoSai.create({
-            nomeCompleto,
-            horaSai: now.format('HH:mm:ss'),
-            data: now.toISOString() // Usa o formato ISO para garantir precisão
-        });
-        res.redirect('/');
-    } catch (error) {
-        console.error('Erro ao registrar saída:', error);
-        res.status(500).send('Erro ao registrar saída');
-    }
-});
 
 // Listagem de pontos de entrada
 app.get('/pontos-ent', async (req, res) => {
@@ -86,21 +66,7 @@ app.get('/pontos-ent', async (req, res) => {
     }
 });
 
-// Listagem de pontos de saída
-app.get('/pontos-sai', async (req, res) => {
-    try {
-        const pontosSai = await PontoSai.findAll();
-        const formattedPontosSai = pontosSai.map(ponto => ({
-            ...ponto.toJSON(),
-            data: moment(ponto.data).tz('America/Bahia').format('YYYY-MM-DD'),
-            horaSai: moment(ponto.horaSai, 'HH:mm:ss').format('HH:mm:ss')
-        }));
-        res.render('listPontoSai', { pontosSai: formattedPontosSai });
-    } catch (error) {
-        console.error('Erro ao buscar saídas:', error);
-        res.status(500).send('Erro ao buscar saídas');
-    }
-});
+
 
 // Formulário para editar ponto de entrada
 app.get('/ponto-ent/:id/edit', async (req, res) => {
@@ -141,44 +107,6 @@ app.put('/ponto-ent/:id', async (req, res) => {
     }
 });
 
-// Formulário para editar ponto de saída
-app.get('/ponto-sai/:id/edit', async (req, res) => {
-    try {
-        const pontoSai = await PontoSai.findByPk(req.params.id);
-        if (pontoSai) {
-            pontoSai.data = moment(pontoSai.data).tz('America/Bahia').format('YYYY-MM-DD'); // Ajusta o formato da data
-            res.render('editPontoSai', { pontoSai });
-        } else {
-            res.status(404).send('Saída não encontrada');
-        }
-    } catch (error) {
-        console.error('Erro ao buscar saída para edição:', error);
-        res.status(500).send('Erro ao buscar saída para edição');
-    }
-});
-
-app.put('/ponto-sai/:id', async (req, res) => {
-    try {
-        const { nomeCompleto, horaSai, data } = req.body;
-        const formattedDate = moment.tz(data, 'America/Bahia').toISOString(); // Usa o formato ISO
-
-        if (!moment(formattedDate).isValid()) {
-            throw new Error('Data inválida');
-        }
-        await PontoSai.update(
-            {
-                nomeCompleto,
-                horaSai,
-                data: formattedDate // Usa o formato ISO
-            },
-            { where: { id: req.params.id } }
-        );
-        res.redirect('/pontos-sai');
-    } catch (error) {
-        console.error('Erro ao atualizar saída:', error);
-        res.status(500).send('Erro ao atualizar saída');
-    }
-});
 
 // Excluir ponto de entrada
 app.delete('/ponto-ent/:id', async (req, res) => {
@@ -191,16 +119,6 @@ app.delete('/ponto-ent/:id', async (req, res) => {
     }
 });
 
-// Excluir ponto de saída
-app.delete('/ponto-sai/:id', async (req, res) => {
-    try {
-        await PontoSai.destroy({ where: { id: req.params.id } });
-        res.redirect('/pontos-sai');
-    } catch (error) {
-        console.error('Erro ao excluir saída:', error);
-        res.status(500).send('Erro ao excluir saída');
-    }
-});
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando na porta http://localhost:${PORT}/`);
